6429c0903e307e7700ebb55d436a447d21c6fdf6
ishiyamaism <ishiyama@kaizen-keikaku.co.jp>
2024-04-27 23:25:24 +0900

    コメント追加

--- CSSeries/VB01Adapter.cs
+++ CSSeries/VB01Adapter.cs
@@ -10,8 +10,13 @@ internal class VB01Adapter : ICSSeries
   private VB01Adapter() { }
   public static VB01Adapter Instance { get; } = new VB01Adapter();
 
-
+  // このようにコンポジションする
   private VB01 _vb01 = new VB01();
+
+  /// <summary>
+  /// 現Interfaceのメソッドでラップする形
+  /// </summary>
+  /// <returns></returns>
   public int GetMeasure()
   {
     return _vb01.GetValue();
